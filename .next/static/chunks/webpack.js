f80a6f139058eda4cde4c5624d79c55747264b08
mamertmg <marquezgarcia.a@gmail.com>
2023-07-24 20:45:30 +0200

    added head

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "659f366e47f41636"; }
+/******/ 		__webpack_require__.h = function() { return "fa1ebdff77da0baa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }) {
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="author" content="Codeefly" />
+        <meta name="author" content="marquezalberto" />
         <title>Alberto Márquez</title>
       </Head>
       <PreLoader />
